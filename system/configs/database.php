cc5bcbe86a4ff8a3bd2a9ff11969f165b4dabd9f
Marcel Naeve <confu5ed@serious-pro.de>
2016-03-03 11:48:32 +0100

    added config file with constants for database configuration.

---
+++ system/configs/database.php
@@ -0,0 +1,12 @@
+<?php
+define('DATABASE_PDO_DRIVER',   'mysql');
+define('DATABASE_HOST',         'localhost');
+define('DATABASE_PORT',         '3306');
+
+define('DATABASE_NAME',         '');
+
+define('DATABASE_USER',         '');
+define('DATABASE_PASSWORD',     '');
+
+define('DATABASE_CHARSET',      'utf8');
+define('DATABASE_COLLATE',      'utf8_unicode_ci');
