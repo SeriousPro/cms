4a37100d5724e4850b2d913c0cd8a122fe5da7c4
Marcel Naeve <confu5ed@serious-pro.de>
2016-03-05 16:54:02 +0100

    added more debugging stuff to temporary home plugin.

--- plugins/home/body.php
+++ plugins/home/body.php
@@ -1,2 +1,17 @@
 <?php
 echo Lang::tr("just some dummy text as placeholder..", __PLUGINS_DIR__."/home");
+?>
+
+<div>
+    <strong>$plugin</strong>
+    <pre>
+        <?php var_dump($plugin); ?>
+    </pre>
+</div>
+
+<div>
+    <strong>$params</strong>
+    <pre>
+        <?php var_dump($params); ?>
+    </pre>
+</div>
